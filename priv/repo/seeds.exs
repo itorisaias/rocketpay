d313cb4cd0053b46960474c59c09e41a84df959d
Itor Isaias <info@insignaconsultoria.com.br>
2021-03-05 23:31:49 -0300

    fix format

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,13 +1,14 @@
 alias Rocketpay.{Repo, User, Account}
 
-%{id: user_id} = User.changeset(%{
-  "name" => "Itor Isaias",
-  "age" => "24",
-  "email" => "itor.isaias@gmail.com",
-  "password" => "123456",
-  "nickname" => "itor.isaias",
-})
-|> Repo.insert!()
+%{id: user_id} =
+  User.changeset(%{
+    "name" => "Itor Isaias",
+    "age" => "24",
+    "email" => "itor.isaias@gmail.com",
+    "password" => "123456",
+    "nickname" => "itor.isaias"
+  })
+  |> Repo.insert!()
 
 Account.changeset(%Account{}, %{
   user_id: user_id,
